c9edbff6c9b8703ff595f7a637bac56b692ac805
huycq <caoquanghuy07091997@gmail.com>
2023-02-09 00:11:34 +0700

    C1 - Create Project

---
+++ SanboxApiWebApp/SanboxApiWebApp/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+// Add these lines of code for using static html files
+app.UseFileServer();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ SanboxApiWebApp/SanboxApiWebApp/wwwroot/js/JavaScript.js
@@ -0,0 +1,21 @@
+﻿//#sourceURL=JavaScript.js
+
+var SANBOX = {
+    init: function () {
+        SANBOX.sb1.eventRegister();
+    },
+
+    sb1: {
+        eventRegister: function() {
+            $('.sb1 button').unbind('click').on('click', SANBOX.sb1.eventClick);
+        },
+
+        eventClick: function() {
+            $('.sb1 h2').html("After click button");
+        }
+    },
+};
+
+$(document).ready(function() {
+    SANBOX.init();
+});
